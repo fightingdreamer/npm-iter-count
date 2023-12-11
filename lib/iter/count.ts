0a042a6ffea820a25bb7a0a055a91066d991b4e2
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-11 23:38:37 +0100

    Add function inline doc

--- lib/iter/count.ts
+++ lib/iter/count.ts
@@ -1,9 +1,17 @@
+/**
+  Count items in iterable
+  @returns number of items
+*/
 export function count<T>(iterator: Iterable<T>): number {
   let size = 0;
   for (const _ of iterator) size += 1;
   return size;
 }
 
+/**
+  Count unique items in iterable
+  @returns number of unique items of iterable grouped by item
+*/
 export function countUnique<T>(iterator: Iterable<T>): Map<T, number> {
   const count = new Map<T, number>();
   for (const item of iterator) {
