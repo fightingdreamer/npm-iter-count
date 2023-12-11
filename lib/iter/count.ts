ca51bec952f84169201e23649384a8b0a6c8527b
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-12 00:02:42 +0100

    Fix jsdoc format

--- lib/iter/count.ts
+++ lib/iter/count.ts
@@ -1,7 +1,7 @@
 /**
-  Count items in iterable
-  @returns number of items
-*/
+ * Count items in iterable
+ * @returns number of items
+ */
 export function count<T>(iterator: Iterable<T>): number {
   let size = 0;
   for (const _ of iterator) size += 1;
@@ -9,9 +9,9 @@ export function count<T>(iterator: Iterable<T>): number {
 }
 
 /**
-  Count unique items in iterable
-  @returns number of unique items of iterable grouped by item
-*/
+ * Count unique items in iterable
+ * @returns number of unique items of iterable grouped by item
+ */
 export function countUnique<T>(iterator: Iterable<T>): Map<T, number> {
   const count = new Map<T, number>();
   for (const item of iterator) {
